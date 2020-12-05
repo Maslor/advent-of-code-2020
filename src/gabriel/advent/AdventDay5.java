e6b39ccbd53293bb6fe5ef05671a6e351f2e94a5
Maslor <gffsac@hotmail.com>
2020-12-05 21:11:06 +0000

    day 5 part 2

--- src/gabriel/advent/AdventDay5.java
+++ src/gabriel/advent/AdventDay5.java
@@ -2,15 +2,20 @@ package gabriel.advent;
 
 import java.util.List;
 import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class AdventDay5 {
 	private static List<String> boardingPasses;
-	private static TreeMap<Long, String> seatIds = new TreeMap<>();
+	private static TreeMap<Integer, String> seatIds = new TreeMap<>();
 
-	public static long solveProblem(String filePath) {
+	public static long solveProblem(String filePath, int problemPart) {
 		boardingPasses = Utils.getFileLines(filePath);
 		populateSeatIds();
-		return seatIds.lastKey();
+		if(problemPart == 1) {
+			return seatIds.lastKey();
+		} else {
+			return findMissingSeat();
+		}
 	}
 
 	private static void populateSeatIds() {
@@ -19,14 +24,31 @@ public class AdventDay5 {
 			int row = boardingPassPositions[0];
 			int column = boardingPassPositions[1];
 
-			seatIds.put(new Long(row * 8 + column), boardingPass);
+			seatIds.put(row * 8 + column, boardingPass);
 		}
 	}
 
 	private static Integer[] getPositions(String boardingPass) {
-		String[] positions = boardingPass.replaceAll("([FB])()([RL])", "$1 $3").split(" ");
-		int row = Integer.parseInt(positions[0].replace("F", "0").replace("B", "1"), 2);
-		int column = Integer.parseInt(positions[1].replace("R", "1").replace("L", "0"), 2);
+		String[] positions = boardingPass
+				.replaceAll("([FB])()([RL])", "$1 $3")
+				.split(" ");
+		int row = Integer.parseInt(positions[0]
+				.replace("F", "0")
+				.replace("B", "1"), 2);
+		int column = Integer.parseInt(positions[1]
+				.replace("L", "0")
+				.replace("R", "1"), 2);
 		return new Integer[] {row, column};
+	}
+
+	private static Integer findMissingSeat() {
+		Integer[] ids = seatIds.keySet().toArray(new Integer[0]);
+		int firstSeat = ids[0];
+		int lastSeat = ids[seatIds.size() - 1];
+		AtomicInteger total =
+				new AtomicInteger((lastSeat + firstSeat) * (lastSeat - firstSeat + 1) / 2); //summation
+		seatIds.keySet().stream().forEach(n -> total.addAndGet(-n));
+
+		return total.get();
 	}
 }
