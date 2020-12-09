9e4b8d10699532443a9bb6b74f6ba1813ca82315
Maslor <gffsac@hotmail.com>
2020-12-09 01:51:23 +0000

    day 8 part 2

--- src/gabriel/advent/AdventDay8.java
+++ src/gabriel/advent/AdventDay8.java
@@ -12,9 +12,24 @@ public class AdventDay8 {
 		Set<Integer> executedLines = new HashSet<>();
 
 		int line = 0;
+
+		boolean isMultiverse = false;
+		int savedLine = 0;
+		int savedAccumulator = 0;
+		Set<Integer> savedLines = new HashSet<>();
+		Set<Integer> savedExecutedLines = new HashSet<>();
+
+
 		while (line < commands.size()) {
 			if (!executedLines.add(line)) {
-				break;
+				if (problemPart == 2) {
+					isMultiverse = false;
+					accumulator = savedAccumulator;
+					executedLines = savedExecutedLines;
+					line = savedLine;
+				} else {
+					break;
+				}
 			} else {
 				executedLines.add(line);
 			}
@@ -22,14 +37,30 @@ public class AdventDay8 {
 			int commandValue = Integer.parseInt(arguments[1]);
 			switch (arguments[0]){
 				case "nop":
-					line++;
+					if (problemPart == 2 && !isMultiverse && savedLines.add(line)) {
+						savedLine = line;
+						savedExecutedLines = new HashSet<>(executedLines);
+						savedAccumulator = accumulator;
+						isMultiverse = true;
+						line += commandValue;
+					} else {
+						line++;
+					}
 					break;
 				case "acc":
 					accumulator += commandValue;
 					line++;
 					break;
 				case "jmp":
-					line += commandValue;
+					if (problemPart == 2 && !isMultiverse && savedLines.add(line)) {
+						savedLine = line;
+						savedExecutedLines = new HashSet<>(executedLines);
+						savedAccumulator = accumulator;
+						isMultiverse = true;
+						line++;
+					} else {
+						line += commandValue;
+					}
 					break;
 			}
 		}
